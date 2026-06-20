58606e7cec2abed5b3ea77ce2bd95308162a2de3
Fuertov <kirillcupin17657@gmail.com>
2026-06-20 19:35:07 +0600

    removed trash

--- trithemius.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <vector>
-#include <cstdint>
-
-std::vector<uint8_t> encryptTrithemius(const std::vector<uint8_t>& data, int a, int b);
-std::vector<uint8_t> decryptTrithemius(const std::vector<uint8_t>& data, int a, int b);
